c251d51fe1ab09f5c7eb4d71ec89eb685815a4ef
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-10 18:19:00 +1000

    feat: create Hero Screen

---
+++ app/components/HeroButton.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, TouchableHighlight, View } from 'react-native'
+
+import AppColors from '../config/AppColors'
+
+export default function HeroButton(
+  {
+    text,
+  } : 
+  {
+    text : string,
+  }
+) {
+  const onPress = () => {
+      console.log("Button Pressed");
+      alert("I doth been pressed");
+  }
+  return (
+    <TouchableHighlight style={styles.buttonTouch} onPress={onPress}>
+      <View style={styles.button}><Text style={styles.text}>{text}</Text></View>
+    </TouchableHighlight>
+  )
+}
+
+const styles = StyleSheet.create({
+  buttonTouch: {
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: AppColors.lightAccent,
+    alignItems: "center",
+    paddingVertical: 25,
+    borderRadius: 10,
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+})
---
+++ app/screens/Hero.tsx
@@ -0,0 +1,69 @@
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
+
+import AppScreen from '../components/AppScreen';
+import AppColors from '../config/AppColors';
+import HeroButton from '../components/HeroButton';
+
+export default function Hero() {
+  return (
+    <AppScreen backgroundColor={AppColors.lightShade}>
+      <View style={styles.header}>
+        <Text style={{fontSize: 40, color: AppColors.lightShade}}>CharitEasy</Text>
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.catch}>
+            Charity management has never been easier.
+          </Text>
+          <Text style={styles.shortDesc}>
+            Create and manage thousands of charities with the touch of a button
+          </Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <HeroButton text="Login"/>
+          <HeroButton text="Register"/>
+        </View>
+      </View>
+    </AppScreen>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: AppColors.main,
+    borderRadius: 25,
+    margin: 10
+  },
+
+  container: {
+    flex: 1,
+    backgroundColor: AppColors.lightShade,
+    paddingHorizontal: 30
+  },
+  textContainer: {
+    gap: 10,
+    paddingTop: 15,
+    paddingBottom: 40,
+    
+  },
+  catch: {
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  shortDesc: {
+    fontSize: 16,
+    fontWeight: "500",
+    textAlign: "center",
+    paddingHorizontal: 20,
+  },
+  buttonContainer: {
+    gap: 15,
+
+  },
+})
