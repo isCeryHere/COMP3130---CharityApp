18ad12c2f65606ca7cb39dff67828a2b32fa2833
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-10 19:30:47 +1000

    feat: create default screen layout

---
+++ app/components/AppScreen.tsx
@@ -0,0 +1,31 @@
+import { StyleSheet, SafeAreaView, Platform } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import Constants from 'expo-constants';
+
+import AppColors from '../config/AppColors';
+
+export default function AppScreen(
+  {
+    children, 
+    statusStyle = "auto",
+    backgroundColor = AppColors.lightShade
+  } : 
+  {
+    children:any, 
+    backgroundColor? : string, 
+    statusStyle? : any
+  }) {
+  return (
+    <SafeAreaView style={
+      [
+        {backgroundColor: backgroundColor, flex: 1},
+        Platform.OS === "android" && {marginTop: Constants.statusBarHeight}
+      ]}>
+      <StatusBar style={statusStyle} backgroundColor={backgroundColor} />
+      {children}
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+})
