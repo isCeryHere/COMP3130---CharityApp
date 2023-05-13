fe9fabda40fa004d1a5a95cd4d4644dbbaa5add4
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-13 20:03:46 +1000

    feat: create ErrorText component

---
+++ app/components/ErrorText.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text } from 'react-native'
+
+export default function ErrorText({children}) {
+  return (
+    <Text style={styles.errorText}>{children}</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  errorText:  {
+    fontSize: 12,
+    color: "#db0000",
+    fontWeight: "bold",
+  },
+})
