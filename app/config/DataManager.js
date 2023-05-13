2b4d29e97fbb0e3d672fc09b9e9707207c10f910
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-13 20:04:54 +1000

    feat: change data structure

--- app/config/DataManager.js
+++ app/config/DataManager.js
@@ -1,11 +1,55 @@
 export default class DataManager {
   static myInstance = null;
+  userData = [
+    {
+      userId: 1,
+      firstName: "Rob",
+      lastName: "Boss",
+      email: "rob@gmail.com",
+      dob: "20/12/1998",
+      password: "abc",
+      collections: [
+        {
+          id: 1,
+          name: "Default",
+          creationDate: "28/04/23",
+          categories: [
+            {
+              id: 1,
+              type: "Education",
+              creation: "28/04/23",
+              charities: []
+            },
+            {
+              id: 2,
+              type: "Medication",
+              creation: "28/04/23",
+              charities: []
+            },
+            {
+              id: 3,
+              type: "Cultural",
+              creation: "28/04/23",
+              charities: []
+            },
+            {
+              id: 4,
+              type: "Environmental",
+              creation: "28/04/23",
+              charities: []
+            },
+            {
+              id: 5,
+              type: "Disaster",
+              creation: "28/04/23",
+              charities: []
+            },
+          ]
+        },
+      ]
+    }
+  ]
 
-  data = {
-    users: [
-
-    ]
-  }
 
   static getInstance() {
     if(DataManager.myInstance == null) {
@@ -17,23 +61,19 @@ export default class DataManager {
 
 
   getUser(email) {
-    return this.data.users.filter((user) => user.email == email);
+    return this.userData.filter((user) => user.email == email);
   }
   createUser(user) {
-    this.data.users.push(user);
+    this.userData.push(user);
   }
   generateUserId() {
-    return this.data.users.length+1;
+    return this.userData.length+1;
   }
-  deleteUser(user) {
-    for(let i = this.data.users.length; i >= 0; i--) {
-      if(this.data.users[i] == user)
-      {
-        this.data.users.splice(i, 1);
-        return true;
-      }
-    }
+  deleteUser(id) {
+    this.userData.splice(id,1);
 
-    return false;
+    for(let i = id+1; i < this.userData.length; i++) {
+      this.userData[i].userId = i-1;
+    }
   }
 }
