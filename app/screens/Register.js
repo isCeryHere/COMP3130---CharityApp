cc195430edeab5a4d85ffe7705cae69fcfa52264
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-13 20:00:43 +1000

    feat: create Register Screen with Formik

--- app/screens/Register.js
+++ app/screens/Register.js
@@ -1,206 +1,221 @@
-import { StyleSheet, Text, View, TouchableHighlight, Platform } from 'react-native'
-import { useState } from 'react';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import { useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
+import * as yup from "yup";
 
-import AppColors from '../config/AppColors'
-import AppScreen from '../components/AppScreen'
-import DefaultInputText from '../components/DefaultTextInput';
-import TextLink from '../components/TextLink';
-import DataManager from '../config/DataManager';
+import AppColors from "../config/AppColors";
+import AppScreen from "../components/AppScreen";
+import DefaultTextInput from "../components/DefaultTextInput";
+import TextLink from "../components/TextLink";
+import DataManager from "../config/DataManager";
+import ErrorText from "../components/ErrorText";
 
 export default function Register() {
-  // User Inputs
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordVerify, setPasswordVerify] = useState("");
-  const [date, setDate] = useState(new Date());
+	const validationSchema = yup.object({
+		firstName: yup.string().required("First Name is required"),
+		lastName: yup.string().required("Last Name is required"),
+		dob: yup.string().nullable().required("Date of Birth is required"),
+		email: yup
+			.string()
+			.email("Invalid Email Address")
+			.required("Email is required"),
+		password: yup.string().required("Password is required"),
+		confirmPassword: yup
+			.string()
+			.required("Password Confirmation is required")
+			.oneOf([yup.ref("password"), null], "Passwords must match"),
+	});
 
-  // Visibility
-  const [showDate, setShowDate] = useState(false);
+	// Visibility
+	const [showDate, setShowDate] = useState(false);
+	const [submitted, setSubmitted] = useState(false);
 
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  const inputValidation = () => {
-    let isCorrect = true;
-    // Name Validation
-    if(firstName === "") {
-      console.log("First Name field must be filled");
-      isCorrect = false;
-    }
-    if(lastName === "") {
-      console.log("Last Name field must be filled");
-      isCorrect = false;
-    }
+	const submitRegistration = (values) => {
+		const dm = DataManager.getInstance();
+		if (dm.getUser(values.email) > 0) {
+			alert("Email is already in use");
+			return;
+		}
 
-    // DOB Validation
-    if(date === "") {
-      console.log("DOB field must be filled");
-      isCorrect = false;
-    }
+		const user = {
+			userId: dm.generateUserId(),
+			email: values.email,
+			firstName: values.firstName,
+			lastName: values.lastName,
+			dob: values.dob,
+			password: values.password,
+		};
 
-    // Email Validation
-    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-    if(email === "") {
-      console.log("Email field must be filled")
-      isCorrect = false;
-    }
-    if(reg.test(email) == false) {
-      console.log("Invalid Email Address: ", email);
-      isCorrect = false;
-    }     
+		dm.createUser(user);
+		console.log(user);
+		// navigation.navigate("Account");
+	};
 
-    // Password Validation
-    if(password === "") {
-      console.log("Password field must be filled");
-      isCorrect = false;
-    }
-    if(passwordVerify === "") {
-      console.log("Password Verification field must be filled");
-      isCorrect = false;
-    } else if(password !== "" && password !== passwordVerify) {
-      console.log("Password Verification doesn't match - " + password + ":" + passwordVerify);
-      isCorrect = false;
-    }
-    
-    if(!isCorrect) {
-      alert("Registration Failed")
-      console.log("Screen Shakes")
-    }
+	return (
+		<AppScreen>
+			<Text style={styles.heading}>Sign Up</Text>
+			<Formik
+				initialValues={{
+					firstName: "",
+					lastName: "",
+					dob: "",
+					email: "",
+					password: "",
+					confirmPassword: "",
+				}}
+				onSubmit={submitRegistration}
+				validationSchema={validationSchema}
+			>
+				{({
+					handleChange,
+					handleBlur,
+					handleSubmit,
+					values,
+					errors,
+					setFieldValue,
+				}) => (
+					<View style={styles.form}>
+						<DefaultTextInput
+							placeholder="First Name"
+							onChangeText={handleChange("firstName")}
+							onBlur={handleBlur("firstName")}
+							value={values.firstName}
+						/>
+						{submitted && errors.firstName && (
+							<ErrorText>{errors.firstName}</ErrorText>
+						)}
+						<DefaultTextInput
+							placeholder="Last Name"
+							onChangeText={handleChange("lastName")}
+							onBlur={handleBlur("lastName")}
+							value={values.lastName}
+						/>
+						{submitted && errors.lastName && (
+							<ErrorText>{errors.lastName}</ErrorText>
+						)}
+						<TouchableHighlight
+							onPress={() => setShowDate(true)}
+							style={styles.dateTouchable}
+						>
+							{/* Touchable Highlight doesn't work without this view element */}
+							<View pointerEvents="none">
+								<DefaultTextInput
+									placeholder="Date of Birth"
+									onChangeText={handleChange("dob")}
+									onBlur={handleBlur("dob")}
+									value={values.dob}
+									style={styles.dateText}
+									readOnly
+								/>
+							</View>
+						</TouchableHighlight>
+						{submitted && errors.dob && <ErrorText>{errors.dob}</ErrorText>}
+						{showDate && (
+							<DateTimePicker
+								testID="dateTimePicker"
+								value={Date.parse(values.dob) || new Date()}
+								onBlur={handleBlur("date")}
+								mode="date"
+								display="default"
+								accentColor={AppColors.main}
+								androidVariant="iosClone"
+								onChange={(event, selectedDate) => {
+									setShowDate(false);
+									setFieldValue("dob", selectedDate.toLocaleDateString());
+								}}
+							/>
+						)}
+						<DefaultTextInput
+							placeholder="Email"
+							onChangeText={handleChange("email")}
+							onBlur={handleBlur("email")}
+							value={values.email}
+						/>
+						{submitted && errors.email && <ErrorText>{errors.email}</ErrorText>}
+						<DefaultTextInput
+							placeholder="Password"
+							onChangeText={handleChange("password")}
+							onBlur={handleBlur("password")}
+							value={values.password}
+							secureTextEntry
+						/>
+						{submitted && errors.password && (
+							<ErrorText>{errors.password}</ErrorText>
+						)}
+						<DefaultTextInput
+							placeholder="Confirm Password"
+							onChangeText={handleChange("confirmPassword")}
+							onBlur={handleBlur("confirmPassword")}
+							value={values.confirmPassword}
+							secureTextEntry
+						/>
+						{submitted && errors.confirmPassword && (
+							<ErrorText>{errors.confirmPassword}</ErrorText>
+						)}
 
-    return isCorrect;
-  }
-  const handleSubmit = () => {
-    const dm = DataManager.getInstance();
-    if(!inputValidation()) 
-      return;
-    else if(dm.getUser(email) > 0) {
-      alert("Email is already in use");
-      return;
-    }
-
-    const user = {
-      userId: dm.generateUserId(),
-      email: email,
-      firstName: firstName,
-      lastName: lastName,
-      dob: date.toLocaleDateString(),
-      password: password
-    }
-
-    dm.createUser(user);
-
-    // TODO: Navigate to Home Screen
-    navigation.navigate("Account");
-  }
-
-  return (
-    <AppScreen>
-      <Text style={styles.heading}>Sign Up</Text>
-      <View style={styles.form}>
-        <DefaultInputText
-          placeholder="First Name"
-          state={firstName}
-          setState={setFirstName}
-        />
-        <DefaultInputText 
-          placeholder="Last Name"
-          state={lastName}
-          setState={setLastName}
-        />
-        <TouchableHighlight onPress={() => setShowDate(true)} style={styles.dateTouchable}>
-          {/* Touchable Highlight doesn't work without this view element */}
-          <View pointerEvents="none">
-            <DefaultInputText 
-                placeholder="Date of Birth"
-                state={date.toLocaleDateString()}
-                setState={setDate}
-                style={styles.dateText}
-              />
-          </View>
-        </TouchableHighlight>
-        {showDate && (
-        <DateTimePicker
-          testID="dateTimePicker"
-          value={date}
-          mode="date"
-          display="default"
-          accentColor={AppColors.main}
-          androidVariant="iosClone"
-          onChange={(event, selectedDate) => {
-            setShowDate(false);
-            setDate(selectedDate);
-          }}
-        />
-        )}
-        <DefaultInputText 
-          placeholder="Email"
-          state={email}
-          setState={setEmail}
-        />
-        <DefaultInputText
-          placeholder="Password"
-          state={password}
-          setState={setPassword}
-          secureTextEntry
-        />
-        <DefaultInputText
-          placeholder="Verify Password"
-          state={passwordVerify}
-          setState={setPasswordVerify}
-          secureTextEntry
-        />
-        <TouchableHighlight style={styles.buttonTouchable} onPress={handleSubmit}>
-          <View style={styles.button}><Text style={styles.buttonText}>Sign Up</Text></View>
-      </TouchableHighlight>
-      </View>
-      <View style={styles.switchView}>
-          <Text style={{fontSize: 16, fontWeight: "500"}}>
-            Already have an account?
-          </Text>
-          <TextLink navigateTo="Login">Login</TextLink>
-      </View>
-    </AppScreen>
-  )
+						<TouchableHighlight
+							style={styles.buttonTouchable}
+							onPress={() => {
+								setSubmitted(true);
+								handleSubmit();
+							}}
+						>
+							<View style={styles.button}>
+								<Text style={styles.buttonText}>Sign Up</Text>
+							</View>
+						</TouchableHighlight>
+					</View>
+				)}
+			</Formik>
+			<View style={styles.switchView}>
+				<Text style={{ fontSize: 16, fontWeight: "500" }}>
+					Already have an account?
+				</Text>
+				<TextLink navigateTo="Login">Login</TextLink>
+			</View>
+		</AppScreen>
+	);
 }
 
 const styles = StyleSheet.create({
-  heading: {
-    fontSize: 30,
-    fontWeight: "bold",
-    paddingHorizontal: 30,
-    paddingTop: 40,
-    paddingBottom: 20,
-  },
-  form: {
-    paddingHorizontal: 30,
-    gap: 15,
-  },
-  dateTouchable: {
-    borderRadius: 5,
-  },
-  dateText: {
-    color: "black",
-  },  
-  buttonTouchable: {
-    marginTop: 20,
-    borderRadius: 10,
-  },
-  button: {
-    backgroundColor: AppColors.main,
-    alignItems: "center",
-    paddingVertical: 25,
-    borderRadius: 10,
-  },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: AppColors.lightShade,
-  },
-  switchView: {
-    padding: 40,
-    alignItems: "center",
-  },
-})
+	heading: {
+		fontSize: 30,
+		fontWeight: "bold",
+		paddingHorizontal: 30,
+		paddingTop: 40,
+		paddingBottom: 20,
+	},
+	form: {
+		paddingHorizontal: 30,
+		gap: 10,
+	},
+	dateTouchable: {
+		borderRadius: 5,
+	},
+	dateText: {
+		color: "black",
+	},
+	buttonTouchable: {
+		marginTop: 20,
+		borderRadius: 10,
+	},
+	button: {
+		backgroundColor: AppColors.main,
+		alignItems: "center",
+		paddingVertical: 25,
+		borderRadius: 10,
+	},
+	buttonText: {
+		fontSize: 18,
+		fontWeight: "bold",
+		color: AppColors.lightShade,
+	},
+	switchView: {
+		padding: 40,
+		alignItems: "center",
+	},
+});
