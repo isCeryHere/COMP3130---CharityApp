714d0a94213241cf7dc6720401ec515538f3657e
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-17 13:36:27 +1000

    feat: add icons

--- app/navigation/TabNavigator.js
+++ app/navigation/TabNavigator.js
@@ -1,18 +1,56 @@
 import { StyleSheet } from "react-native";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
+
+import CollectionNavigator from "./CollectionNavigator";
 import Charities from "../screens/Charities";
 import Account from "../screens/Account";
-import CollectionNavigator from "./CollectionNavigator";
+import AppColors from "../config/AppColors";
+import Create from "../screens/Create";
+
 
 export default function TabNavigator() {
-  const Tab = createBottomTabNavigator();
-
+	const Tab = createBottomTabNavigator();
+	const iconSize = 35;
 	return (
 		<Tab.Navigator screenOptions={{ headerShown: false }}>
-			<Tab.Screen name="Collection" component={CollectionNavigator} />
-			<Tab.Screen name="Charities" component={Charities} />
-			<Tab.Screen name="Account" component={Account} />
+			<Tab.Screen
+				name="Collection"
+				component={CollectionNavigator}
+				options={{
+					tabBarIcon: () => (
+						<Ionicons name="folder" size={iconSize} color={AppColors.main} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Charities"
+				component={Charities}
+				options={{
+					tabBarIcon: () => (
+						<Ionicons name="cash" size={iconSize} color={AppColors.main} />
+					),
+				}}
+			/>
+			<Tab.Screen 
+				name="Create"
+				component={Create}
+				options={{
+					tabBarIcon: () => (
+						<Ionicons name="create" size={iconSize} color={AppColors.main} />
+					),
+				}}
+			/>
+			<Tab.Screen
+				name="Account"
+				component={Account}
+				options={{
+					tabBarIcon: () => (
+						<Ionicons name="person-circle" size={iconSize} color={AppColors.main} />
+					),
+				}}
+			/>
 		</Tab.Navigator>
 	);
 }
