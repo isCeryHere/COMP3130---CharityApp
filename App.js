1f8223bb2a65d099bad2b2449e66ceed06d5f70e
Cery <100125995+isCeryHere@users.noreply.github.com>
2023-05-19 17:46:21 +1000

    feat: remove unused elements

--- App.js
+++ App.js
@@ -2,10 +2,6 @@ import { StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
-import CollectionNavigator from "./app/navigation/CollectionNavigator";
-import Home from "./app/screens/Home";
-import Account from "./app/screens/Account";
-import Collection from "./app/screens/Collection";
 
 export default function App() {
   return (
@@ -15,11 +11,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
